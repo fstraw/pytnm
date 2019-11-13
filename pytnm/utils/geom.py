c17f0537b19b12659556d18cbda268c301738a3e
brbatt <brbatt@hntb.com>
2019-11-13 09:41:05 -0500

    Removed scratch tests for inclusion in test suite

--- pytnm/utils/geom.py
+++ pytnm/utils/geom.py
@@ -37,13 +37,7 @@ def update_feature_z(fc, raster):
             cursor.updateRow([newpolyline])
 
 if __name__ == '__main__':
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)    
-    test_receiver = "../../tests/test_files/DATA/receiver.shp"
-    test_existing_roadway = "../../tests/test_files/DATA/existing_roadway.shp"
-    test_barrier = "../../tests/test_files/DATA/barrier.shp"
-    raster = r"C:\Users\brbatt\Documents\elevation_NED10M_ga121_3765948_01\elevation\ned10m33084h4.tif"
-    update_feature_z(test_barrier, raster)
+    pass
 
 
 
--- pytnm/utils/stamina.py
+++ pytnm/utils/stamina.py
@@ -163,11 +163,4 @@ def write_stamina_file(file_path, condition, roadways=None, barriers=None, recei
     return file_path
 
 if __name__ == '__main__':
-    test_existing_roadway = "../../tests/test_files/DATA/existing_roadway.shp"
-    test_barrier = "../../tests/test_files/DATA/barrier.shp"
-    test_receiver = "../../tests/test_files/DATA/receiver.shp"
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    os.chdir(dir_path)    
-    test_road_geom = [row[0] for row in SearchCursor(test_existing_roadway, "SHAPE@")][0]
-    test_barrier_geom = [row[0] for row in SearchCursor(test_barrier, "SHAPE@")][0]
-    print(write_stamina_file(r"C:\TNM25\Program", "EXISTING", test_existing_roadway, test_barrier, test_receiver))
+    pass
