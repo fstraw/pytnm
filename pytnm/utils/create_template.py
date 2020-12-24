b2411c766b397355935fe925089e8086b4a63838
Brandon Batt <brbatt@brandons-mbp.lan>
2020-12-24 09:41:37 -0500

    Refactored create_template.py

--- pytnm/utils/create_template.py
+++ pytnm/utils/create_template.py
@@ -95,267 +95,55 @@ def create_barrier(input_folder):
 
 def create_terrain_line(input_folder):
     f = "terrain_line"
-    writer.shapefile2.Writer(os.path.join(input_folder), f), shapeType=shapefile2.POLYLINEZ)
-    writer.field("name", "C", 32)
-    writer.field("project", "C", 100)
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POLYLINEZ)
+    writer.field("name", "C", size=32)
+    writer.field("project", "C", size=100)
 
+def create_study_area(input_folder):
+    f = "study_area"
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POLYGONZ)
+    writer.field("name", "C", size=32)
+    writer.field("project", "C", size=100)
 
-# def create_study_area(input_folder):
-#     study_area = cf(out_path=input_folder,
-#         out_name="study_area",
-#         geometry_type="POLYGON",
-#         template=None,
-#         has_m="ENABLED",
-#         has_z="ENABLED",
-#         spatial_reference=None
-#     )
-#     af(in_table=study_area, 
-#         field_name="name", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="name",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
+def create_study_receivers(input_folder):
+    f = "study_receiver"
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POINTZ)
+    writer.field("rec_id", "C", size=10)
+    writer.field("x", "N")
+    writer.field("y", "N")
+    writer.field("z", "N")
+    writer.field("project", "C", size=100)
 
-# def create_study_receivers(input_folder):
-#     receivers = cf(out_path=input_folder,
-#         out_name="study_receiver",
-#         geometry_type="POINT",
-#         template=None,
-#         has_m="ENABLED",
-#         has_z="ENABLED",
-#         spatial_reference=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="rec_id", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="rec_id",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="x", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="x",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="y", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="y",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="z", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="z",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
+def create_field_measurements(input_folder):
+    f = "field_measurement"
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POINTZ)
+    writer.field("rec_id", "C", size=10)
+    writer.field("x", "N")
+    writer.field("y", "N")
+    writer.field("z", "N")
+    writer.field("dba", "N")
+    writer.field("project", "C", size=100)
 
-# def create_field_measurements(input_folder):
-#     receivers = cf(out_path=input_folder,
-#         out_name="field_measurement",
-#         geometry_type="POINT",
-#         template=None,
-#         has_m="ENABLED",
-#         has_z="ENABLED",
-#         spatial_reference=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="rec_id", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="rec_id",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="x", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="x",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="y", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="y",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="z", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="z",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="dba", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="dba",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=receivers, 
-#         field_name="project", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="project",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-# def create_ground_zone(input_folder):
-#     ground_zone = cf(out_path=input_folder,
-#         out_name="ground_zone",
-#         geometry_type="POLYGON",
-#         template=None,
-#         has_m="ENABLED",
-#         has_z="ENABLED",
-#         spatial_reference=None
-#     )
-#     af(in_table=ground_zone, 
-#         field_name="name", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="name",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=ground_zone, 
-#         field_name="type", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=15,
-#         field_alias="type",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-# def create_tree_zone(input_folder):
-#     tree_zone = cf(out_path=input_folder,
-#         out_name="tree_zone",
-#         geometry_type="POLYGON",
-#         template=None,
-#         has_m="ENABLED",
-#         has_z="ENABLED",
-#         spatial_reference=None
-#     )
-#     af(in_table=tree_zone, 
-#         field_name="name", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="name",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=tree_zone, 
-#         field_name="avg_hgt", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="avg_hgt",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-# def create_building_row(input_folder):
-#     building_row = cf(out_path=input_folder,
-#         out_name="building_row",
-#         geometry_type="POLYLINE",
-#         template=None,
-#         has_m="ENABLED",
-#         has_z="ENABLED",
-#         spatial_reference=None
-#     )
-#     af(in_table=building_row, 
-#         field_name="name", 
-#         field_type="TEXT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=10,
-#         field_alias="name",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=building_row, 
-#         field_name="avg_hgt", 
-#         field_type="DOUBLE",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="avg_hgt",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
-#     af(in_table=building_row, 
-#         field_name="bldg_pct", 
-#         field_type="SHORT",
-#         field_precision=None,
-#         field_scale=None, 
-#         field_length=None,
-#         field_alias="bldg_pct",
-#         field_is_nullable="NULLABLE",
-#         field_is_required="NON_REQUIRED",
-#         field_domain=None
-#     )
+def create_ground_area(input_folder):
+    f = "ground_zone"
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POLYGONZ)
+    writer.field("name", "C", size=32)
+    writer.field("type", "C", size=32)
+    writer.field("project", "C", size=100)
 
-# if __name__ == '__main__':
-#     pass
+def create_tree_zone(input_folder):
+    f = "tree_zone"
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POLYGONZ)
+    writer.field("name", "C", size=32)
+    writer.field("avg_hgt", "N")
+    writer.field("project", "C", size=100)
 
-create_receivers(r"C:\Users\brbatt\projects\pytnm\tests")
+def create_building_row(input_folder):
+    f = "builing_row"
+    writer = shapefile2.Writer(os.path.join(input_folder, f), shapeType=shapefile2.POLYLINEZ)
+    writer.field("name", "C", size=32)
+    writer.field("avg_hgt", "N")
+    writer.field("bldg_pct", "N")
+
+if __name__ == '__main__':
+    pass
