6a8d4fb7e44b68649134212ee97e0b782b21a54f
fstraw <brbatt@gmail.com>
2020-12-28 11:27:22 -0500

    Bumped to version 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pytnm",
-    version="1.0.1",
+    version="1.0.2",
     description="FHWA TNM 2.5 Geospatial Toolkit",
     long_description=README,
     long_description_content_type="text/markdown",
